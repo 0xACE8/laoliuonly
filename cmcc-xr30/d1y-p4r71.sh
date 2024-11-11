1a3e830a599caa7ea4f8966a15b19b29e9c4d48d
0xACE8 <19428059+0xACE8@users.noreply.github.com>
2024-11-10 23:51:54 -0800

    Update d1y-p4r71.sh

--- cmcc-xr30/d1y-p4r71.sh
+++ cmcc-xr30/d1y-p4r71.sh
@@ -18,18 +18,30 @@ sed -i '15 iCONFIG_TARGET_DEVICE_PACKAGES_mediatek_mt7981_DEVICE_cmcc_xr30-emmc=
 sed -i '16 iCONFIG_TARGET_DEVICE_mediatek_mt7981_DEVICE_cmcc_xr30=y' defconfig/mt7981-ax3000.config
 sed -i '17 iCONFIG_TARGET_DEVICE_PACKAGES_mediatek_mt7981_DEVICE_cmcc_xr30=""' defconfig/mt7981-ax3000.config
 
-sed -i '253 i\\tcmcc,xr30* \|\\' target/linux/mediatek/mt7981/base-files/lib/upgrade/platform.sh
-sed -i '213 i\\tcmcc,xr30-emmc \|\\' target/linux/mediatek/mt7981/base-files/lib/upgrade/platform.sh
-sed -i '206 i\\tcmcc,xr30 \|\\' target/linux/mediatek/mt7981/base-files/lib/upgrade/platform.sh
 
-sed -i '758 i\\t*cmcc,xr30* \|\\' package/mtk/applications/mtk-smp/files/smp.sh
+# target/linux/mediatek/mt7981/base-files/lib/upgrade/platform.sh
+sed -i '/cmcc,rax3000m\*/a\\tcmcc,xr30* \|\\' target/linux/mediatek/mt7981/base-files/lib/upgrade/platform.sh
+sed -i '/cmcc,rax3000m-emmc/a\\tcmcc,xr30-emmc \|\\' target/linux/mediatek/mt7981/base-files/lib/upgrade/platform.sh
+sed -i '/cmcc,rax3000m /a\\tcmcc,xr30 \|\\' target/linux/mediatek/mt7981/base-files/lib/upgrade/platform.sh
 
-sed -i '19 i\\tcmcc,xr30-emmc \|\\' target/linux/mediatek/mt7981/base-files/lib/preinit/90_extract_caldata
+# package/mtk/applications/mtk-smp/files/smp.sh
+sed -i '/rax3000m/i\\t\*cmcc,xr30\* \|\\' package/mtk/applications/mtk-smp/files/smp.sh
 
+# target/linux/mediatek/mt7981/base-files/lib/preinit/90_extract_caldata
+sed -i '/cmcc,rax3000m-em)/i\\tcmcc,xr30-emmc \|\\' target/linux/mediatek/mt7981/base-files/lib/preinit/90_extract_caldata
+
+# target/linux/mediatek/files-5.4/arch/arm64/boot/dts/mediatek/mt7981-cmcc-rax3000m.dtsi
 sed -i 's/mt7981.dtsi/mt7981-cmcc-rax3000m-xr30.dtsi/g' target/linux/mediatek/files-5.4/arch/arm64/boot/dts/mediatek/mt7981-cmcc-rax3000m.dtsi
 
-sed -i '227 i\\tcmcc,xr30-emmc \|\\' target/linux/mediatek/mt7981/base-files/etc/board.d/02_network
-sed -i '48 i\\t*cmcc,xr30* \|\\' target/linux/mediatek/mt7981/base-files/etc/board.d/02_network
+# target/linux/mediatek/mt7981/base-files/etc/board.d/02_network
+sed -i 's/lan_mac=\$(mmc_get_mac_binary factory 0x24)/lan_mac=\$(mmc_get_mac_binary factory 0x2a)/g' target/linux/mediatek/mt7981/base-files/etc/board.d/02_network
+sed -i 's/wan_mac=\$(mmc_get_mac_binary factory 0x2a)/wan_mac=\$(mmc_get_mac_binary factory 0x24)/g' target/linux/mediatek/mt7981/base-files/etc/board.d/02_network
+sed -i '/cmcc,rax3000m-emmc)/i\\tcmcc,xr30-emmc\|\\' target/linux/mediatek/mt7981/base-files/etc/board.d/02_network
+sed -i '/\*rax3000m\*)/i\\t\*cmcc,xr30\* \|\\' target/linux/mediatek/mt7981/base-files/etc/board.d/02_network
+sed -i '/\*rax3000m\*)/{n;d}' target/linux/mediatek/mt7981/base-files/etc/board.d/02_network
+sed -i '/\*rax3000m\*)/a\\t\t\t"0:lan:3" "1:lan:2" "2:lan:1" "6u@eth0"' target/linux/mediatek/mt7981/base-files/etc/board.d/02_network
+sed -i '/\*rax3000m\*)/a\\t\tucidef_add_switch "switch0" \\' target/linux/mediatek/mt7981/base-files/etc/board.d/02_network
+sed -i '/\*rax3000m\*)/a\\t\tucidef_set_interfaces_lan_wan "eth0" "eth1"' target/linux/mediatek/mt7981/base-files/etc/board.d/02_network
 
 
 cat >> ./target/linux/mediatek/image/mt7981.mk <<-"EOF"
